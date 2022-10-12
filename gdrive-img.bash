a289591be1191110bce372af6c5b07b8c96615bc
Oliver Lenehan <oli@sunsetkookaburra.xyz>
2022-10-12 20:39:59 +1100

    gdrive download script

--- gdrive-img.bash
+++ gdrive-img.bash
@@ -2,22 +2,59 @@
 
 TARGET="public/img"
 
-mkdir -p "$TARGET"
-
-echo "Listing Google Drive folder ..."
-gdrive="$(curl -sSf 'https://drive.google.com/drive/folders/1_qviIV62bztqS3ALrsvd5RQR1k7DYhPp')"
-
-files="$(paste <(echo "$gdrive" | htmlq -a data-id '.iZmuQc>c-wiz>div') <(echo "$gdrive" | htmlq -t '.iZmuQc>c-wiz>div'))"
-
-echo "$files" | {
-  while IFS=$'\t' read id name rest
-  do
-    echo "Downloading $name"
-    curl -sSfL "https://drive.google.com/uc?id=$id" > "$TARGET/$name"
-    if [ "$?" != 0 ]
-    then
-      echo "curl failed"
-      exit 1
-    fi
-  done
+is_folder () {
+  if grep -qiv 'folder'
+  then
+    echo "folder"
+  else
+    echo "file"
+  fi
 }
+
+fetch_folder () {
+  local target_dir="$1"
+  local folder_id="$2"
+
+  mkdir -p "$target_dir"
+
+  local gfolder="$(curl -sSf "https://drive.google.com/drive/folders/$2")"
+
+  local n_files="$(echo "$gfolder" | htmlq '.iZmuQc>c-wiz>div' | wc -l)"
+
+  # <ID> \t <NAME> \t <FOLDERTYPE>
+  local files="$(
+    paste \
+      <(echo "$gfolder" | htmlq -a data-id '.iZmuQc>c-wiz>div' | cat) \
+      <(echo "$gfolder" | htmlq -a data-tooltip '.iZmuQc>c-wiz>div div[data-tooltip]' | cat) \
+      <(echo "$gfolder" | htmlq '.iZmuQc>c-wiz>div' | is_folder)
+  )"
+
+  if [ "$n_files" = 0 ]
+  then
+    echo "Empty folder"
+    return
+  else
+    echo "Folder contains $n_files items"
+  fi
+
+  echo "$files" | {
+    local id name foldertype rest
+    while IFS=$'\t' read id name foldertype rest
+    do
+      echo "Downloading $foldertype \`$name\`"
+      if [ "$foldertype" = "folder" ]
+      then
+        fetch_folder "$target_dir/$name" "$id"
+      else
+        curl -sSfL "https://drive.google.com/uc?id=$id" > "$target_dir/$name"
+        if [ "$?" != 0 ]
+        then
+          echo "curl failed"
+          exit 1
+        fi
+      fi
+    done
+  }
+}
+
+fetch_folder "$TARGET" "1_qviIV62bztqS3ALrsvd5RQR1k7DYhPp"
